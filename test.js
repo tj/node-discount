424de969f8c6cad099b6d26f96784a212d5a11a5
Tj Holowaychuk <tj@vision-media.ca>
2010-09-21 13:32:29 -0700

    Fixed tests for latest node

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
 
-var assert = require('assert'),
-    fs = require('fs'),
-    markdown = require('./build/markdown')
+var assert = require('assert')
+  , fs = require('fs')
+  , markdown = require('./build/default/markdown');
 
 assert.ok(markdown.version.match(/^\d+\.\d+\.\d+$/), 'version is improperly formatted')
-assert.equal(fs.readFileSync('test/test.html'), markdown.parse(fs.readFileSync('test/test.md')))
+assert.equal(
+    fs.readFileSync('test/test.html', 'utf8')
+  , markdown.parse(fs.readFileSync('test/test.md', 'utf8'))
+  );
+
+console.log('all good');
